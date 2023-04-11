f49fedc110f7b9b70d9de1fa03435302893bfdd9
accPenguin <125560812+accPenguin@users.noreply.github.com>
2023-04-11 14:39:55 +0800

    Add files via upload

---
+++ src/components/BestWish.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+
+withDefaults(defineProps<{
+  name: string
+  msg: string
+  art: string
+}>(), {
+  name: 'my friend',
+  msg: 'Happy Birthday!',
+  art: 'Life advice: Donot rely on anyone, do what you want to do, cherish relationships between people, donot be afraid of embarrassment, start from imperfection, control imperfection, make unimportant things imperfect, and believe that you are fully prepared,think before you think,do dome execise on monday night',
+})
+</script>
+
+<template>
+   <meta charset="UTF-8">
+  <div class="wish transition animate-slow animate-fadeIn" text="center">
+    <h1 text="3xl" font="light" m="2">
+      {{ name }}
+    </h1>
+    <h2 text="3xl" font="normal">
+      {{ msg }}
+    </h2>
+    <h3 text="3xl" font="normal">
+      {{ art }}
+    </h3>
+  </div>
+</template>
+
+<style lang="scss">
+.wish{
+  color: #8b6a60;
+  font-family: "Lato", "Roboto" sans-serif;
+}
+</style>
---
+++ src/utils/egg.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+console.log(
+  '%c Happy Birthday! ',
+  'background-color:#93B7F3;color:white;padding:2px;',
+)
+console.log(
+  '%c 虽然又老了一岁，但祝你新的一岁更加开心！',
+  'background-color:dodgerblue;color:white;padding:2px;',
+)
+console.log(
+  '%c 来自你的朋友 %c Kylin Feng ',
+  'background-color:#000000;color:white;padding:3px;',
+  'border: 1px solid black;padding:2px;',
+)
